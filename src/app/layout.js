989db9c8f60fb5e51f5a172f04d2ac498def23c4
T JAIPRAKASH <t.jaiprakash20@gmail.com>
2025-10-18 23:29:20 +0530

    google analytics add on

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -19,9 +20,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" love-deals="879BC0364EB9EBEE3DBE71B15E175613">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        {/* ✅ Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-VEPY8TJGTN"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VEPY8TJGTN');
+          `}
+        </Script>
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
